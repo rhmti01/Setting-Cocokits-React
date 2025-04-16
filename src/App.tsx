f7511afac2611a7a34e2792cb503574c745dfc91
rhmti01 <rahmati.business@gmail.com>
2025-04-17 00:31:44 +0330

    rebuild app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { ThemeConfigContext } from "@cocokits/react-components";
 import { framesXThemeConfig } from '@cocokits/theme-frames-x';
-import { FormField, Label, Input , IconButton , SvgIcon } from "@cocokits/react-components";
+import { Button, Checkbox , Toggle  } from '@cocokits/react-components';
 
 import './App.css'
-
 
 function App() {
   return (
     <ThemeConfigContext.Provider value={framesXThemeConfig}>
-     <FormField size="md">
-        <Label>Label</Label>
-        <Input placeholder="Placeholder" />
-      </FormField>
-      <IconButton type="default" size="md" color="brand">
-      <SvgIcon icon="YOUR_ICON" />
-    </IconButton>
+      <h1>cocokits react</h1>
+      <Button>Hello</Button>
+      <Checkbox>This is a checkbox</Checkbox>
+      
+
+      <Toggle type="knob" size="md" labelPosition="before">
+        Slide Me!
+      </Toggle>
+
     </ThemeConfigContext.Provider>
   );
 }
